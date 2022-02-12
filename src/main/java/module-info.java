6ec7aca0328c47620eb2464dcd0c795bf48e7a2b
JWaters02 <watersjoshua2002@gmail.com>
2022-02-12 11:21:41 +0000

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module dev.jwaters.hacknotts21 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens dev.jwaters.hacknotts21 to javafx.fxml;
+    exports dev.jwaters.hacknotts21;
+}
