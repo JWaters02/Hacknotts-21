185b3d38814b8b9fa62c352775a0fbb238d85e13
samuelyoung-uk <75908355+samuelyoung-uk@users.noreply.github.com>
2022-02-13 11:36:48 +0000

    made function display its params

--- src/main/java/dev/jwaters/hacknotts21/codecorrectness/TypeChecker.java
+++ src/main/java/dev/jwaters/hacknotts21/codecorrectness/TypeChecker.java
@@ -33,7 +33,11 @@ public class TypeChecker {
                 // dont check on void types
                 child.getReturnType(containingFunc) != VoidType.INSTANCE &&
                 !Objects.equals(child.getReturnType(containingFunc), node.getExpectedChildType(child, containingFunc)
-            )) return new Pair<>(node, String.format("expected %s got %s", node.getExpectedChildType(child, containingFunc), child.getReturnType(containingFunc)));
+            )) return new Pair<>(node, String.format(
+                    "on %s expected %s got %s",
+                    child,
+                    node.getExpectedChildType(child, containingFunc),
+                    child.getReturnType(containingFunc)));
 
             var childValid = isValid(child, containingFunc);
             if (childValid != null) return childValid;
